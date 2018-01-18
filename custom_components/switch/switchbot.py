180415bbdb7c3d50f218e85692621e8fab308894
Isabella <isabella.alstrom@gmail.com>
2018-01-18 21:20:02 +0100

    functioning switchbot custom component, but no error handling

--- custom_components/switch/switchbot.py
+++ custom_components/switch/switchbot.py
@@ -1,6 +1,7 @@
 """
 Switch bot
 """
+import asyncio
 import logging
 import voluptuous as vol
 
@@ -11,16 +12,24 @@ from homeassistant.const import (STATE_OFF, STATE_ON, CONF_NAME, CONF_SWITCHES)
 import binascii
 from bluepy.btle import Peripheral
 
-_LOGGER = logging.getLogger(__name__)
+import homeassistant.helpers.config_validation as cv
 
-DOMAIN = 'switchbot'
 """ key's expected from user configuration"""
 CONF_NAME = 'name'
 CONF_MAC = 'mac'
 
+DOMAIN = 'switchbot'
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+      vol.Required(CONF_MAC): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+_LOGGER = logging.getLogger(__name__)
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Find and return Sensibo data"""
-    my_name = config.get(CONF_NAME)    
+    my_name = config.get(CONF_NAME, 'switchbot')    
     my_mac = config.get(CONF_MAC)
     add_devices([SwitchBot(my_name, my_mac)])
     _LOGGER.info("Tried to setup SWITCH BOT.")
@@ -28,12 +37,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class SwitchBot(SwitchDevice):
     """Representation of a Switch Bot."""
 
-    def __init__(self, name, given_api_key):
-        _LOGGER.info("Initialized Switch Bot SWITCH %s", name)
-        self._name = name
-        self._mac = given_api_key
+    def __init__(self, my_name, my_mac):
+        _LOGGER.info("Initialized Switch Bot SWITCH %s", my_name)
+        self._name = my_name
+        self._mac = my_mac
         self._state = False
-        # self.update()
 
     def turn_on(self, **kwargs):
         """Turn device on."""
@@ -43,7 +51,8 @@ class SwitchBot(SwitchDevice):
         hand_service = p.getServiceByUUID("cba20d00-224d-11e6-9fb8-0002a5d5c51b")
         hand = hand_service.getCharacteristics("cba20002-224d-11e6-9fb8-0002a5d5c51b")[0]
         hand.write(binascii.a2b_hex("570101"))
-        hass.states.set('sensor.stefan_radiator', 'on')
+        self._state = 'on'
+        self.hass.states.set('sensor.' + self._name, 'on')
 
     def turn_off(self, **kwargs):
         """Turn device off."""
@@ -53,14 +62,14 @@ class SwitchBot(SwitchDevice):
         hand_service = p.getServiceByUUID("cba20d00-224d-11e6-9fb8-0002a5d5c51b")
         hand = hand_service.getCharacteristics("cba20002-224d-11e6-9fb8-0002a5d5c51b")[0]
         hand.write(binascii.a2b_hex("570102"))
-        hass.states.set('sensor.' + entity_id, 'off')
+        self._state = 'off'
+        self.hass.states.set('sensor.' + self._name, 'off')
 
-    # def update(self):
-    #     _LOGGER.debug("Checking Sensibo SWITCH stats for  %s", self._name)
-    #     client = SensiboClientAPI(self._givent_api_key)
-    #     devices = client.devices()
-    #     uid = devices[self._name]
-    #     _LOGGER.debug("Sensibo uid =%s", uid)
-    #     """Get the latest settings from the thermostat."""
-    #     current_settings = client.pod_ac_state(uid)
-    #     self._state = current_settings['on']
+    @property
+    def is_on(self):
+        """Return true if device is on."""
+        return self._state
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return self._name
